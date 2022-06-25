36780e8d82bd1237fcaab2f5a3d5dd8c904ba947
Rashmisingh-18 <rashmi134copy@gmail.com>
2022-06-25 16:18:50 +0530

    Changes in form

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -28,9 +28,9 @@ function  Navbar() {
       <div className="logo" class="float-left text-blue-100 m-6 text-2xl uppercase">Esurancy</div>   
 
       <div class=" mr-4 navbar-container-text p-2 flex justify-end space-x-16 w-100 items-baseline"> 
-        <div class="hover:underline cursor-pointer underline-offset-8 hover:underline decoration-1" onClick={handleClaimForm}>FEED INSURANCE DATA</div>
+        <div class="hover:underline cursor-pointer underline-offset-8 hover:underline decoration-1" onClick={handleFeedInsuranceData}>FEED INSURANCE DATA</div>
         <div class="hover:underline cursor-pointer underline-offset-8 hover:underline decoration-1" onClick={handleInsuranceDataForm}>SEARCH INSURANCE DATA</div>
-        <div class="hover:underline cursor-pointer underline-offset-8 hover:underline decoration-1" onClick={handleFeedInsuranceData}>CLAIM INSURANCE</div>
+        <div class="hover:underline cursor-pointer underline-offset-8 hover:underline decoration-1" onClick={handleClaimForm}>CLAIM INSURANCE</div>
         <div> <input type="text" placeholder="Search" class="navbar-searchfield"></input></div>
       </div>
     </div>
