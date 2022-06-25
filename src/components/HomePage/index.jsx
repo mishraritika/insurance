d4cc820f4196007c067f9c741606b16a0d899b0f
mishraritika <78527919+mishraritika@users.noreply.github.com>
2022-06-25 18:01:06 +0530

    Revert "Changes made"

--- src/components/HomePage/index.jsx
+++ src/components/HomePage/index.jsx
@@ -2,16 +2,15 @@ import React from 'react'
 
 function HomePage() {
   return (
-    <>
-    <section class="HeroSection grid grid-cols-2 divide-x flex-wrap mt-6">
-        <div class="HeroSectionDetails mt-8 mr-8 pt-8">
-            <h1 class="text-slate-50 index-h1 mb-6">We are your trusted insurance agency</h1>
-            <h3 class="text-blue-100 index-p">Life insurance offers you Long-term Savings which will give huge benefit later, feel allowed to make inquiry</h3>
-        </div>
-        <img class="image w-[50vw]"
-            src="familyprotection.jpeg"/> 
-    </section>
-    </>  
+    <div>
+          <div class="flex mt-16">
+              <div class="w-100 order-1 basis-1/2">kfjebedkfbjke</div>
+              <div class="order-2 basis-1/2 align-middle">
+          <img src="familyprotection.jpeg" class="w-[30vw]"/>
+          </div>
+          </div>
+          
+    </div>
   )
 }
 
